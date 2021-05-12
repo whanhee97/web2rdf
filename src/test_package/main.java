4420b667518bbb768a9bd5663fc595b15a5badaa
hhlee <whanhee97@naver.com>
2021-05-12 22:24:48 +0900

    Coreference Resolution

--- src/test_package/main.java
+++ src/test_package/main.java
@@ -1,16 +1,24 @@
 package test_package;
+import edu.stanford.nlp.coref.CorefCoreAnnotations;
+import edu.stanford.nlp.coref.data.CorefChain;
+import edu.stanford.nlp.coref.data.Dictionaries;
+import edu.stanford.nlp.coref.data.Mention;
 import edu.stanford.nlp.ie.util.RelationTriple;
 import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
 import edu.stanford.nlp.naturalli.OpenIE;
 import edu.stanford.nlp.naturalli.SentenceFragment;
 import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreEntityMention;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.IntPair;
 import edu.stanford.nlp.util.PropertiesUtils;
 import org.apache.jena.rdf.model.*;
 import org.jsoup.Jsoup;
@@ -88,16 +96,66 @@ public class main {
 
         // stanford OpenIE
         Properties props = PropertiesUtils.asProperties(
-                "annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie"
+                "annotators", "tokenize,ssplit,pos,lemma,ner,parse,coref"
         );
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
         String text;
         if (args.length > 0) {
             text = IOUtils.slurpFile(args[0]);
         } else {
-            text = bodyText;
+            text = "Korea (officially the \"Korean Peninsula\") is a region in East Asia. Since 1945 it has been divided into the two parts which soon became the two sovereign states: North Korea (officially the \"Democratic People's Republic of Korea\") and South Korea (officially the \"Republic of Korea\"). Korea consists of the Korean Peninsula, Jeju Island, and several minor islands near the peninsula. It is bordered by China to the northwest and Russia to the northeast. It is separated from Japan to the east by the Korea Strait and the Sea of Japan (East Sea). During the first half of the 1st millennium, Korea was divided between the three competing states of Goguryeo, Baekje, and Silla, together known as the Three Kingdoms of Korea.";
+            //text = bodyText;
         }
+
+
         Annotation docu = new Annotation(text);
+        pipeline.annotate(docu);
+        List<String> sentList = new ArrayList<>();
+        for (CoreMap sentence : docu.get(CoreAnnotations.SentencesAnnotation.class)) {
+            sentList.add(sentence.get(CoreAnnotations.TextAnnotation.class));
+        }
+
+
+
+        String newText = "";
+        Collection<CorefChain> values = docu.get(CorefCoreAnnotations.CorefChainAnnotation.class).values();
+        for (CorefChain cc : values) {
+            //System.out.println("\t" + cc.getMentionsInTextualOrder());
+            List<CorefChain.CorefMention> mentionsInTextualOrder = cc.getMentionsInTextualOrder();
+            String coreWord = "";
+            for (int i = 0; i < mentionsInTextualOrder.size(); i++){
+                if (i == 0){
+                    coreWord = mentionsInTextualOrder.get(i).mentionSpan; // 첫번째 명사를 원래 명사로 지정
+                }
+                String mention = mentionsInTextualOrder.get(i).mentionSpan; // 대명사 가져오기
+                int sentNum = mentionsInTextualOrder.get(i).sentNum -1; //문장 번호 가져오기
+                String modiSent = sentList.get(sentNum); // 수정될 문장 가져오고
+                modiSent = modiSent.replaceAll(mention,coreWord); // mention(대명사를) coreWord(원래단어)로 바꿔주고
+                sentList.set(sentNum,modiSent); // 수정된 문자열로 바꿔줌
+            }
+        }
+
+        //System.out.println(sentList);
+
+        for (String s : sentList) {
+            newText += s + " ";
+        }
+        System.out.println(text);
+        System.out.println("--------------------------------------------");
+        System.out.println(newText);
+
+        System.out.println("\n \n");
+
+
+        props = PropertiesUtils.asProperties(
+                "annotators", "tokenize,ssplit,pos,lemma,parse,natlog,openie"
+        );
+        props.setProperty("openie.max_entailments_per_clause","100");
+        props.setProperty("openie.triple.strict","false");
+        pipeline = new StanfordCoreNLP(props);
+
+        docu = new Annotation(newText);
         pipeline.annotate(docu);
         int sentNo = 0;
         for (CoreMap sentence : docu.get(CoreAnnotations.SentencesAnnotation.class)) {
@@ -124,7 +182,6 @@ public class main {
 //            }
 //            System.out.println();
         }
-
 
     }
     //map 정렬 메소드
